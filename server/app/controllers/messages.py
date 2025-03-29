35e0db759a1b9b0549a8277bd97368e8be5c84fd
jinyang628 <chenjinyang4192@gmail.com>
2025-03-29 19:54:26 -0400

    Pass Clerk JWT token in headers

--- server/app/controllers/messages.py
+++ server/app/controllers/messages.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Header, HTTPException
 
 from app.models.message import MessageRequest, MessageResponse
 from app.services.messages import MessagesService
@@ -21,9 +21,12 @@ class MessagesController:
             "",
             response_model=MessageResponse,
         )
-        async def chat(
-            input: MessageRequest,
-        ) -> MessageResponse:
+        async def chat(input: MessageRequest, authorization: str = Header(...)) -> MessageResponse:
+            if not authorization.startswith("Bearer "):
+                raise HTTPException(status_code=401, detail="Invalid token format")
+
+            token: str = authorization.removeprefix("Bearer ").strip()
+
             log.info(f"Sending message of id {input.id} to assistant...")
             response: MessageResponse = await self.service.chat(input=input)
             log.info("Message to be sent back to user: %s", response.content)
