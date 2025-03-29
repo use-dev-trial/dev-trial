c88abd81b70772e33ac289eb2ff3fd2c09fb01e4
SuveenE <suveen.te1@gmail.com>
2025-03-29 13:15:13 -0700

    update

--- frontend/src/components/create-challenge/question-preview.tsx
+++ frontend/src/components/create-challenge/question-preview.tsx
@@ -104,7 +104,7 @@ export default function QuestionPreview({
                 <p className="text-md font-semibold text-slate-800">Files</p>
               </div>
               <div className="divide-y divide-slate-100 rounded-lg border border-slate-100 bg-white shadow-sm">
-                {question?.files?.files?.map((file, index) => (
+                {question?.files?.map((file, index) => (
                   <div key={index} className="p-5">
                     <h3 className="mb-3 flex items-center font-medium text-slate-800">
                       <span className="mr-2 inline-block h-2 w-2 rounded-full bg-emerald-500"></span>
