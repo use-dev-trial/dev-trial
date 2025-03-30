93ed2781e01cd4c948f8de910442e54eb917e6aa
jinyang628 <chenjinyang4192@gmail.com>
2025-03-29 21:06:18 -0400

    Better client init

--- server/app/controllers/messages.py
+++ server/app/controllers/messages.py
@@ -1,10 +1,11 @@
 import logging
 
 from fastapi import APIRouter, Depends
+from supabase._async.client import AsyncClient as Client
 
 from app.models.message import MessageRequest, MessageResponse
 from app.services.messages import MessagesService
-from app.utils.dependencies import verify_token
+from app.utils.dependencies import init_db_client
 
 log = logging.getLogger(__name__)
 
@@ -23,9 +24,9 @@ class MessagesController:
             response_model=MessageResponse,
         )
         async def chat(
-            input: MessageRequest, token: str = Depends(verify_token)
+            input: MessageRequest, client: Client = Depends(init_db_client)
         ) -> MessageResponse:
             log.info(f"Sending message of id {input.id} to assistant...")
-            response: MessageResponse = await self.service.chat(input=input, token=token)
+            response: MessageResponse = await self.service.chat(input=input, client=client)
             log.info("Message to be sent back to user: %s", response.content)
             return response
--- server/app/utils/database.py
+++ server/app/utils/database.py
@@ -1,11 +1,10 @@
 import logging
 import os
 import uuid
-from typing import Optional
 
+from supabase import AsyncClientOptions
 from supabase._async.client import AsyncClient as Client
 from supabase._async.client import create_client
-from supabase import AsyncClientOptions
 
 log = logging.getLogger(__name__)
 
@@ -18,47 +17,21 @@ def is_valid_uuid(value):
         return False
 
 
-class DatabaseManager:
-    _instance: Optional["DatabaseManager"] = None
-    client: Client = None  # type: ignore
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    async def _init(self, token: str):
-        if self.client is not None:
-            return
-
-        try:
-            supabase_url = os.environ["SUPABASE_URL"]
-            supabase_key = os.environ["SUPABASE_ANON_KEY"]
-            self.client = await create_client(
-                supabase_url=supabase_url,
-                supabase_key=supabase_key,
-                options=AsyncClientOptions(
-                    headers={
-                        "Authorization": f"Bearer {token}",
-                        "apiKey": supabase_key,
-                    }
-                ),
-            )
-            log.info("Supabase client initialized.")
-        except KeyError as e:
-            log.error(f"Missing environment variable: {e}")
-            raise
-        except Exception as e:
-            log.error(f"Error initializing Supabase client: {e}")
-            raise
-
-    @classmethod
-    async def get_instance(cls, token: str) -> "DatabaseManager":
-        if cls._instance is None:
-            cls._instance = cls()
-
-        await cls._instance._init(token=token)
-        return cls._instance
+async def db_client(
+    token: str,
+) -> Client:
+    supabase_url = os.environ["SUPABASE_URL"]
+    supabase_key = os.environ["SUPABASE_ANON_KEY"]
+    return await create_client(
+        supabase_url=supabase_url,
+        supabase_key=supabase_key,
+        options=AsyncClientOptions(
+            headers={
+                "Authorization": f"Bearer {token}",
+                "apiKey": supabase_key,
+            }
+        ),
+    )
 
 
 """
