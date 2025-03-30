9b8735742595ececd74547a3f8bdc513177193f0
SuveenE <suveen.te1@gmail.com>
2025-03-30 10:19:26 -0700

    Add create_challege endpoint

--- server/app/controllers/challenges.py
+++ server/app/controllers/challenges.py
@@ -1,11 +1,10 @@
 import logging
 
-from fastapi import APIRouter, Depends
-from supabase._async.client import AsyncClient as Client
-
 from app.models.challenge import Challenge
 from app.services.challenges import ChallengesService
 from app.utils.dependencies import init_db_client
+from fastapi import APIRouter, Depends
+from supabase._async.client import AsyncClient as Client
 
 log = logging.getLogger(__name__)
 
@@ -29,6 +28,20 @@ class ChallengesController:
             log.info(f"Getting challenge with id {challenge_id}...")
             response: Challenge = await self.service.get_challenge(
                 challenge_id=challenge_id, client=client
+            )
+            log.info("Challenge: %s", response)
+            return response
+
+        @router.post(
+            "/",
+            response_model=Challenge,
+        )
+        async def create_challenge(
+            challenge: Challenge, client: Client = Depends(init_db_client)
+        ) -> Challenge:
+            log.info("Creating challenge: %s", challenge)
+            response: Challenge = await self.service.create_challenge(
+                challenge=challenge, client=client
             )
             log.info("Challenge: %s", response)
             return response
--- server/app/services/challenges.py
+++ server/app/services/challenges.py
@@ -1,8 +1,8 @@
 import logging
 
-from supabase._async.client import AsyncClient as Client
-
 from app.models.challenge import Challenge
+from app.models.database import Table
+from supabase._async.client import AsyncClient as Client
 
 log = logging.getLogger(__name__)
 
@@ -10,3 +10,23 @@ log = logging.getLogger(__name__)
 class ChallengesService:
     async def get_challenge(self, client: Client, challenge_id: str) -> Challenge | None:
         pass
+
+    async def create_challenge(self, client: Client, challenge: Challenge) -> Challenge:
+        insert_challenge_result = (
+            await client.table(Table.CHALLENGES)
+            .insert(
+                {
+                    "name": challenge.name,
+                    "description": challenge.description,
+                    "problem_id": challenge.problem_id,
+                }
+            )
+            .execute()
+        )
+        return Challenge(
+            id=insert_challenge_result.data[0]["id"],
+            name=challenge.name,
+            description=challenge.description,
+            problem_id=challenge.problem_id,
+        )
+
