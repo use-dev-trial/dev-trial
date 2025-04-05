4dea3fecefe801ea4c5828269fe0c5cba9647080
SuveenE <suveen.te1@gmail.com>
2025-04-04 20:33:27 -0700

    Update layout.tsx

--- arena/src/app/layout.tsx
+++ arena/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { ThemeProvider } from 'next-themes';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import '../globals.css';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
