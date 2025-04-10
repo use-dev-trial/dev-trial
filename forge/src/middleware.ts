fa27e57d941866f8df8af9a74476d51ef5aaef7b
jinyang628 <chenjinyang4192@gmail.com>
2025-04-10 12:58:43 -0400

    Losser config match so dynamic route isnt being left out

--- forge/src/middleware.ts
+++ forge/src/middleware.ts
@@ -42,8 +42,5 @@ export default clerkMiddleware(
 );
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!_next|favicon.ico).*)'],
 };
