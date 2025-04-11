8d7c5c1f4148115aa377b9a8a00743001cce3a44
SuveenE <suveen.te1@gmail.com>
2025-04-10 23:12:48 -0700

    Add use-all-questions hook

--- forge/src/components/challenges/home/questions-tab.tsx
+++ forge/src/components/challenges/home/questions-tab.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import { useAllQuestions } from '@/hooks/questions/use-all-question';
 import { PlusCircle } from 'lucide-react';
 
 import AddQuestionDialog from '@/components/challenges/home/add-question-dialog';
@@ -10,33 +11,37 @@ import { Card, CardContent } from '@/components/ui/card';
 
 export default function QuestionsTab({ challengeId }: { challengeId: string }) {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [questions, setQuestions] = useState<string[]>([]);
+  const { questions } = useAllQuestions({
+    challengeId: challengeId,
+  });
 
-  // TODO: Fetch questions for the challenge using useQuestion hook
-
-  const handleAddQuestion = (question: string) => {
-    setQuestions([...questions, question]);
-    setIsDialogOpen(false);
-  };
+  console.log(questions);
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center p-8 text-center">
-        <p className="text-muted-foreground mb-6">
-          Create questions for your challenge {challengeId}
-        </p>
-        <Button className="bg-blue-500 hover:bg-blue-600" onClick={() => setIsDialogOpen(true)}>
-          <PlusCircle className="mr-2 h-4 w-4" />
-          Add Question
-        </Button>
-      </div>
+      {questions.length === 0 && (
+        <div className="flex flex-col items-center justify-center p-8 text-center">
+          <p className="text-muted-foreground mb-6">
+            Create questions for your challenge {challengeId}
+          </p>
+          <Button className="bg-blue-500 hover:bg-blue-600" onClick={() => setIsDialogOpen(true)}>
+            <PlusCircle className="mr-2 h-4 w-4" />
+            Add Question
+          </Button>
+        </div>
+      )}
 
       {questions.length > 0 && (
         <div className="mt-6 grid gap-4">
           {questions.map((question, index) => (
             <Card key={index}>
               <CardContent className="p-4">
-                <p>{question}</p>
+                <p>Title</p>
+                <p>{question.problem.title}</p>
+                <p>Test Cases</p>
+                <p>{question.test_cases.map((test_case) => test_case.description).join(', ')}</p>
+                <p>Files</p>
+                <p>{question.files.map((file) => file.code).join(', ')}</p>
               </CardContent>
             </Card>
           ))}
@@ -46,7 +51,7 @@ export default function QuestionsTab({ challengeId }: { challengeId: string }) {
       <AddQuestionDialog
         isOpen={isDialogOpen}
         onClose={() => setIsDialogOpen(false)}
-        onAddQuestion={handleAddQuestion}
+        onAddQuestion={() => {}}
       />
     </div>
   );
