cd6edc743f8d09c7e7cb0c5b1d97eb6c8404b5ca
jinyang628 <chenjinyang4192@gmail.com>
2025-04-12 16:08:28 -0400

    better schema for metrics

--- server/app/services/grading.py
+++ server/app/services/grading.py
@@ -6,4 +6,17 @@ from app.models.grading import Metric
 
 class GradingService:
     async def upsert_metric(self, client: Client, input: Metric):
-        await client.table(Table.METRICS).upsert(input.model_dump()).execute()
+        if input.id:
+            await client.table(Table.METRICS).update(
+                {
+                    "question_id": input.question_id,
+                    "content": input.content,
+                }
+            ).eq("id", input.id).execute()
+        else:
+            await client.table(Table.METRICS).insert(
+                {
+                    "question_id": input.question_id,
+                    "content": input.content,
+                }
+            ).execute()
